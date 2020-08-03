45d906ffb474f3f626d1c3a431833b1e8408639d
BolinChen <chenberlin1998@gmail.com>
2020-08-03 16:58:38 +0800

    《剑指offer》部分题目解答

--- src/Practice/JZ57.java
+++ src/Practice/JZ57.java
@@ -1,5 +1,9 @@
 package Practice;
-
+//二叉树的下一个节点
+//题目描述
+//给定一个二叉树和其中的一个结点，请找出中序遍历顺序的下一个结点并且返回。注意，树中的结点不仅包含左右子结点，同时包含指向父结点的指针。
+//知识点：树
+//思路：若pNode有右子树则输出右子树的最左节点，若pNode无右子树则输出第一个满足pNode在其左子树中的父辈节点。
 public class JZ57 {
     public TreeLinkNode GetNext(TreeLinkNode pNode) {
         //如果节点为空则返回空
